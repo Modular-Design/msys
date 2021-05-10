aaab4aeb5d66894483465b518f057f34574b149a
willi-z <willi.w.zschiebsch@web.de>
2021-05-10 17:44:56 +0200

    fixes support for lower python version

--- src/mdd/modules/__init__.py
+++ src/mdd/modules/__init__.py
@@ -26,7 +26,7 @@ def get_modules():
 
     # search in entry points
     import sys
-    if sys.version_info < (3, 10):
+    if sys.version_info < (3, 8):
         from importlib_metadata import entry_points
     else:
         from importlib.metadata import entry_points
